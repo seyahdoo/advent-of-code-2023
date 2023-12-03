c4ae33dba42f2cc9e878c2d7b385ed3d40c544b9
Seyyid Dogan <seyahdoo@gmail.com>
2023-12-03 23:51:44 +0300

    day one part one

---
+++ day_one_part_one.py
@@ -0,0 +1,19 @@
+﻿import os
+
+with open("input_day_one.txt", encoding="utf-8") as f:
+    lines = f.readlines()
+    sum = 0
+    for line in lines:
+        num1 = 0
+        for c in line:
+            if c.isnumeric():
+                num1 = int(c)
+                break
+        reverse = line[::-1]
+        num2 = 0
+        for c in reverse:
+            if c.isnumeric():
+                num2 = int(c)
+                break
+        sum += (num1 * 10) + num2
+    print(sum)
---
+++ day_one_part_two.py
@@ -0,0 +1,27 @@
+﻿import re
+
+with open("input_day_one.txt", encoding="utf-8") as f:
+    lines = f.readlines()
+
+regex = "(one)|(two)|(three)|(four)|(five)|(six)|(seven)|(eight)|(nine)|\\d"
+numbers = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+sum = 0
+
+def string_to_value(s:str):
+    if(s[0].isnumeric()):
+        return (int)(s[0])
+    for i in range(1, 10):
+        if(s == numbers[i]):
+            return i
+    return None
+
+for line in lines:
+    matches = []
+    for match in re.finditer(regex, line):
+        matches.append(match)
+    if len(matches) <= 0:
+        continue
+    start_val = string_to_value(matches[0].group())
+    end_val = string_to_value(matches[-1].group())
+    sum += (start_val * 10) + end_val
+print(sum)
