b12e0e699244768bc91fa65c0a246530fe53a0fa
Seyyid Dogan <seyahdoo@gmail.com>
2023-12-14 10:29:45 +0100

    day 14 part 2 wip

---
+++ day_14/part2.py
@@ -0,0 +1,67 @@
+﻿
+
+def main():
+    with open("input.txt", encoding="utf-8") as f:
+        lines = f.readlines()
+        lines = list(map(lambda x: x.strip().strip("\ufeff"), lines))
+    lines = read_debug_input()
+    rock_matrix = list(map(lambda x: list(x), lines))
+    rock_matrix = roll_to_south(rock_matrix)
+    print(calculate_north_load(rock_matrix))
+    return
+
+def read_debug_input():
+    debug_input = """
+        O....#....
+        O.OO#....#
+        .....##...
+        OO.#O....O
+        .O.....O#.
+        O.#..O.#.#
+        ..O..#O..O
+        .......O..
+        #....###..
+        #OO..#....
+        """
+    return debug_input.strip().split()
+
+def roll_to_north(rock_matrix):
+    for i in range(len(rock_matrix)):
+        changed = False
+        for x in range(1, len(rock_matrix)):
+            for y in range(len(rock_matrix[0])):
+                if rock_matrix[x][y] == "O" and rock_matrix[x-1][y] == ".":
+                    rock_matrix[x][y] = "."
+                    rock_matrix[x-1][y] = "O"
+                    changed = True
+        if not changed:
+            break
+    return rock_matrix
+
+def roll_to_south(rock_matrix):
+    for i in range(len(rock_matrix)):
+        changed = False
+        for x in range(len(rock_matrix) - 1, 0, -1):
+            for y in range(len(rock_matrix[0])):
+                if rock_matrix[x][y] == "O" and rock_matrix[x+1][y] == ".":
+                    rock_matrix[x][y] = "."
+                    rock_matrix[x+1][y] = "O"
+                    changed = True
+        if not changed:
+            break
+    return rock_matrix
+
+
+
+def calculate_north_load(rock_matrix):
+    total_weight = 0
+    l = len(rock_matrix)
+    for x in range(l):
+        weight = l-x
+        for y in range(len(rock_matrix[0])):
+            if rock_matrix[x][y] == "O":
+                total_weight += weight
+    return total_weight
+
+if __name__ == '__main__':
+    main()        
